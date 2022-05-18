22d5e328a0d13a21b617b2d88a3115ddf1cd2ff3
jorgesmunoz <jorgesmunoz@gmail.com>
2022-05-18 08:35:21 -0300

    personal portfolio

---
+++ personal_portfolio/blog/models.py
@@ -0,0 +1,12 @@
+from pyexpat import model
+from django.db import models
+
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
